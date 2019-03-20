a8b0fb54be67439c58c49ff7142bceb71cfae346
Jacek <jacek@karczmarczyk.pl>
2019-03-20 16:02:05 +0700

    fix: passing version to addTag

--- bin/cz-add-tag.js
+++ bin/cz-add-tag.js
@@ -3,8 +3,11 @@
 const yargv = require('yargs').argv;
 const packageJsonPath = yargv._[0] || './package.json';
 const addTag = require('../lib/addTag');
+const version = JSON.parse(fs.readFileSync(packageJsonPath, {
+  encoding: 'utf-8',
+})).version;
 
-addTag(packageJsonPath).then(tag => {
+addTag(version).then(tag => {
   console.log(`[cz-deploy] Added tag "${tag}".`);
 }).catch(e => {
   console.error(`[cz-deploy] ${e.message}`);

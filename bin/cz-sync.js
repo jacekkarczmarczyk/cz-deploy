d0f989decc1bbe96b75f0034ba9ae9c120b82606
Jacek <jacek@karczmarczyk.pl>
2019-03-23 19:28:58 +0700

    feat: cz-sync

---
+++ bin/cz-sync.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const mergeBranches = require('../lib/syncBranches');
+const yargv = require('yargs').argv;
+const devBranch = yargv.dev || 'dev';
+const masterBranch = yargv.dev || 'master';
+const push = yargv.push;
+const pushTags = yargv.pushTags;
+
+mergeBranches(devBranch, masterBranch, push, pushTags).then(() => {
+  console.log(`[cz-deploy] Synced${push ? ' and pushed ' : ' '}${devBranch} and ${masterBranch} branches`);
+}).catch(e => {
+  console.error(`[cz-deploy] ${e.message}`);
+  throw e;
+});

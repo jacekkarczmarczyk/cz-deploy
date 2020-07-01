5946572cb3d663069264e43194dde07e8af41fc1
Jacek Karczmarczyk <jkarczm@gmail.com>
2020-07-01 09:48:52 +0200

    feat: cz-commit can now also push and push-tags

--- bin/cz-commit.js
+++ bin/cz-commit.js
@@ -2,14 +2,21 @@
 
 const fs = require('fs');
 const commit = require('../lib/commit');
+const mergeBranches = require('../lib/syncBranches');
 const yargv = require('yargs').argv;
 const packageJsonPath = yargv._[0] || './package.json';
 const chore = yargv['chore'] || 'release';
-const version = JSON.parse(fs.readFileSync(packageJsonPath, {
-  encoding: 'utf-8',
-})).version;
+const push = yargv.push;
+const pushTags = yargv.pushTags;
+const devBranch = yargv.dev || 'dev';
+const masterBranch = yargv.dev || 'master';
+const version = JSON.parse(fs.readFileSync(packageJsonPath, { encoding: 'utf-8' })).version;
 
-commit(version, chore).then(({ commitMessage, tag }) => {
+commit(version, chore).then(async ({ commitMessage, tag }) => {
+  if (push || pushTags) {
+    await mergeBranches(devBranch, masterBranch, push, pushTags);
+    console.log(`[cz-deploy] Synced${push ? ' and pushed ' : ' '}${devBranch} and ${masterBranch} branches`);
+  }
   console.log(`[cz-deploy] Commited "${commitMessage}", added tag "${tag}".`);
 }).catch(e => {
   console.error(`[cz-deploy]`, e);

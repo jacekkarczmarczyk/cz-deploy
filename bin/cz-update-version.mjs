6c5d95bdc0b7e42dd0c83fe66a7890f4d50a25d6
Jacek Karczmarczyk <jkarczm@gmail.com>
2023-08-05 09:36:59 +0200

    feat: cz-update-version --dry-run

--- bin/cz-update-version.mjs
+++ bin/cz-update-version.mjs
@@ -2,10 +2,14 @@
 
 import updateVersion from '../lib/updateVersion.mjs';
 import getSettings from '../lib/getSettings.mjs';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 
+const yargv = yargs(hideBin(process.argv)).argv;
+const { dryRun } = yargv;
 const settings = await getSettings();
 
-updateVersion('./package.json', settings.productionBranch, settings.developmentBranch).then(({ commits, next, previous }) => {
+updateVersion('./package.json', settings.productionBranch, settings.developmentBranch, dryRun).then(({ commits, next, previous }) => {
   console.log(`[cz-deploy] ${commits.length} commit(s) since v${previous}:`);
   commits.forEach(line => console.log(`[cz-deploy] COMMIT ${line}`));
   console.log(`[cz-deploy] Updated ./package.json to v${next}.`);
